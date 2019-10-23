ed144e94f49e4478b863e2962caf104d74f68659
KWIF Studios <56926486+KWIF@users.noreply.github.com>
2019-10-23 09:01:37 -0600

    Delete Classes Blue.py

--- Term1/Classes Blue.py
+++
@@ -1,9 +0,0 @@
-#Blue Day Classes
-print("8am-Programming Python 1 & 2")
-print("10:30am-MathBlock")
-print("11:40am-Lunch")
-print("12:15-Study Hall")
-print("1:30-Gym")
-print("2:40-End Of The Day")
-                       
-                       
