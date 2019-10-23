f804509dbd1d13e241df60c53e5203f9166d15d3
KWIF Studios <56926486+KWIF@users.noreply.github.com>
2019-10-23 09:03:27 -0600

    Delete Var.py

--- Term1/Var.py
+++
@@ -1,7 +0,0 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> playerScore = 100 #string to display on screen
->>> petName = "Rusty"
->>> bookNUM = 12000
->>> cont_ans = True #bool for a yes or no answer
->>> triArea = #float formula using divison
